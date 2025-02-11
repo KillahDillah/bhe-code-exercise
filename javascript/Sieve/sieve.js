7e1b5f4512b860da136dc81ad13e329d9b39eb11
Leah Miller <leahmiller1969@gmail.com>
2025-02-11 13:19:18 -0800

    attempts

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,27 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+    const min = 2;
+    let primes = [];
+
+    let isPrime = true;
+    let end = n * 15;
+
+    for (let i = min; i < end; i++) {
+      for (let j = min; j < Math.sqrt(end); j++) {
+        if (i !== j && i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primes.push(i);
+      }
+      isPrime = true;
+    }
+    if (n <= min) {
+      primes.push(2);
+    }
+    return primes[n];
   }
 }
 
